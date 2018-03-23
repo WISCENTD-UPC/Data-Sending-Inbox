930d177b643c58b99d248761d3280c93667f8a86
Alexis Rico <sferadev@gmail.com>
2018-03-23 10:16:50 +0100

    Update imports

--- src/main/java/wiscentd/dhis2/inbox/InboxServlet.java
+++ src/main/java/wiscentd/dhis2/inbox/InboxServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import java.io.*;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
